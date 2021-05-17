7d45e4ab07892e86fb836efafa5b92085dc7ce81
Kevin Oh <aflashyrhetoric@gmail.com>
2021-05-16 21:26:47 -0400

    IMprove refactor over to controller patterngs

--- app.js
+++ app.js
@@ -2,41 +2,15 @@ import express from "express"
 import ProductsHandler from "./src/handlers/ProductsHandler"
 import JobsHandler from "./src/handlers/JobsHandler"
 
-// import { fetchZyteData } from "./src/zyte"
-
-const findProductsWithNullValues = data =>
-  data.filter(checkIfProductHasNullKeys)
-
-const checkIfProductHasNullKeys = product =>
-  Object.entries(product).some(([key, value]) => value === null)
-
-// **************
-// ***************
-// Initialize Express
-// ***************
 const app = express()
 
-// ***************
+// ******************
 // Routes / Endpoints
-// ***************
+// ******************
 app.get("/refresh_product_data/:runNumber", JobsHandler.RefreshProductData)
+
 app.get("/fetch_product_data", ProductsHandler.getProductData)
-app.get("/fetch_null_product_data", async (req, res, next) => {
-  let data = await redis.get("product_data")
-  // console.log(data)
-
-  data = JSON.parse(data)
-  data = findProductsWithNullValues(data)
-  // console.log(data)
-
-  res.setHeader("Access-Control-Allow-Origin", "*")
-
-  res.json({
-    status: 200,
-    data,
-  })
-})
-
+app.get("/fetch_null_product_data", ProductsHandler.getNullProductData)
 app.listen(3210, () => {
   console.log(`running on port 3210`)
 })
--- src/handlers/ProductsHandler.js
+++ src/handlers/ProductsHandler.js
@@ -1,14 +1,29 @@
 import init from "./initHandler"
 export default class ProductsHandler {
-  static async getProductData(req, res, next) {
+  static async getProductData(req, res) {
     const { redis } = init()
     const data = await redis.get("product_data")
 
     res.setHeader("Access-Control-Allow-Origin", "*")
-
     res.json({
       status: 200,
       data,
+    })
+  }
+
+  static async getNullProductData(req, res) {
+    const { redis } = init()
+    const data = await redis.get("product_data")
+
+    const checkIfProductHasNullKeys = product =>
+      Object.entries(product).some(([key, value]) => value === null)
+    const findProductsWithNullValues = data =>
+      data.filter(checkIfProductHasNullKeys)
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.json({
+      status: 200,
+      data: findProductsWithNullValues(JSON.parse(data)),
     })
   }
 
