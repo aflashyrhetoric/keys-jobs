40353c91020809aa00a8d017f80b8193d31fb1a4
Kevin Oh <cmyk.k4@gmail.com>
2021-07-01 04:00:06 -0400

    add builds as amodel

--- src/model/Build.js
+++ src/model/Build.js
@@ -2,18 +2,18 @@ import db from "./db"
 // Model is responsible for calling db related functions
 export default class Build {
   static getAll() {
-    return db.select().from("products")
+    return db.select().from("builds")
   }
   static saveNewProduct(product) {
-    return db.insert(product, ["id"]).into("products")
+    return db.insert(product, ["id"]).into("builds")
   }
   static saveUpdatesToProduct(sku, updatedProduct) {
     return db
       .where("sku", "=", sku)
       .update(updatedProduct, ["sku"])
-      .into("products")
+      .into("builds")
   }
   static deleteProduct(sku, updatedProduct) {
-    return db("products").where("sku", "=", sku).delete()
+    return db("builds").where("sku", "=", sku).delete()
   }
 }
